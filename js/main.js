52b1e1632af6baea36a3ec4586e5d1c75eacbb94
jesusgabriel <amelo@fvi.edu>
2016-10-12 21:39:43 -0400

    'terminado'

---
+++ js/main.js
@@ -0,0 +1,58 @@
+var video = $("video")[0];
+
+function sizeControl() {
+  var videoWidth = $("video").width();
+  $('#video-control').css('width', videoWidth+'px');
+
+}
+window.onload = sizeControl;
+window.onresize =  sizeControl;
+loadState();
+
+$("#backward").click(function() {
+var video = $("video")[0];
+  if (!video.paused){
+    video.currentTime = Math.max(0, video.currentTime-10);
+  }
+});
+$("#forward").click(function() {
+var video = $("video")[0];
+  if (!video.paused){
+    video.currentTime = Math.max(0, video.currentTime+10);
+  }
+});
+$("#arrow-up").click(function() {
+var video = $("video")[0];
+  if (!video.paused){
+    video.playbackRate += 0.25;
+  }
+});
+$("#arrow-down").click(function() {
+
+  if (!video.paused){
+    video.playbackRate -= 0.25;
+  }
+});
+$("#button").click(function(){
+  if (!$("#search").val()) return;
+  video.src= $("#search").val();
+});
+
+function saveState(){
+  console.log("mhgccgn");
+localStorage.setItem("lastPlayed", video.src);
+localStorage.setItem("lastLocation", video.currentTime);
+}
+setInterval(saveState, 1000);
+
+function loadState() {
+  if (!localStorage.getItem("lastPlayed")|| !localStorage.getItem("lastLocation"))
+    return;
+  video.src = localStorage.getItem("lastPlayed");
+  video.play()
+  .then(()=>video.currentTime = localStorage.getItem("lastLocation"))
+  .then(()=>video.pause());
+
+
+
+}
